5f39dd3c4f914d13cbed9d37a063b9b32ff895c5
Starlik <pavel.bolt@bk.ru>
2023-10-20 22:28:19 +0300

    каждое животное издаёт свой звук

--- src/main/java/org/softwaretechnologies/animals/AnimalType.java
+++ src/main/java/org/softwaretechnologies/animals/AnimalType.java
@@ -2,11 +2,20 @@ package org.softwaretechnologies.animals;
 
 public enum AnimalType {
     CAT{
-
+        @Override
+        public String toString() {
+            return "meow";
+        }
     }, DOG{
-
+        @Override
+        public String toString() {
+            return "woof";
+        }
     }, COW{
-
+        @Override
+        public String toString() {
+            return "moo";
+        }
     };
 
 
