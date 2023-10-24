829664c5e794c8c4cf250f09a7c30fa09993920a
Starlik <pavel.bolt@bk.ru>
2023-10-24 17:51:48 +0300

    Поднял версию Mpckito. Всё работает

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.mockito:mockito-inline:4.9.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
 }
 
 sourceSets {
